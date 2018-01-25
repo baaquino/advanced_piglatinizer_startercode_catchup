80e0138ea078353d1b695ed36bfe0596693c0c7a
Bench Aquino <2019017@icacademy.org>
2018-01-25 00:01:45 +0000

    function

--- script.js
+++ script.js
@@ -7,10 +7,7 @@
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkway.
 
 $(document).ready(function() {
-    $("#translate").click(function() {
-        var typedWord = $("#word").val();
-        $("#output").append(typedWord)
-    });
+    
     /*
 
     */
@@ -41,14 +38,15 @@ $(document).ready(function() {
             return false;}
         
     }
-    //var testWord2 = word;
-    //var testResult2 = wordStartsWithVowel(testWord2);
+    var testWord2 = word;
+    var testResult2 = wordStartsWithVowel(testWord2);
     
     // Appends "yay" to the end of the word and returns the word.
     function appendYayToWord(word) {
         return word + "yay";
     }
-
+    var aWord = word;
+    var result = appendYayToWord(aWord); 
     // Moves the first consonant to the end of the word, appends "ay" to the end of the word,
     // and returns the word.
     function convertWordWithConsonant(word) {
@@ -56,22 +54,33 @@ $(document).ready(function() {
         var sub= word.substring(1);
         return sub + con + "ay";
     }
-    //var testWord4 = word;
-    //var testResult4 = convertWordWithConsonant(testWord4);
+    var testWord4 = word;
+    var testResult4 = convertWordWithConsonant(testWord4);
     
     // If the word starts with a vowel, return the result of appendYayToWord.
     // Otherwise, return the result of convertWordWithConsonant.
     function convertWordToPigLatin(word) {
-        if((wordStartsWithVowel(word))){
-            return appendYayToWord
+        
+        if (typedWord = testResult2){
+            return result;
         }
         else {
-            convertWordWithConsonant(word);
+            return testResult4;
         }
+        //if((wordStartsWithVowel(word))){
+          //  return result; 
+        //}
+        //else {
+          //  return testResult4;
+        //}
         
     }
-console.log(convertWordToPigLatin("hi"));
+console.log(convertWordToPigLatin);
     // Create a click handler that takes is triggered when the user clicks the translate button.
+        $("#translate").click(function() {
+        var typedWord = $("#word").val();
+        $("#output").append(typedWord)
+    });
         // Gets a word from the input box
         // Transforms the word to pig latin
         // Displays the result on the screen in the output element
